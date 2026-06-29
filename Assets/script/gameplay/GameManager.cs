d523a87f7a7791a5f34edc1a3268271aa9ca0231
najla <najlalaudy@gmail.com>
2026-06-29 10:19:27 +0700

    fix

--- Assets/script/gameplay/GameManager.cs
+++ Assets/script/gameplay/GameManager.cs
@@ -45,8 +45,6 @@ public class GameManager : MonoBehaviour
 
     public void Home()
     {
-        if (isTransitioning) return;
-        isTransitioning = true;
         StartCoroutine(LoadSceneWithDelay("MainMenu"));
     }
 
